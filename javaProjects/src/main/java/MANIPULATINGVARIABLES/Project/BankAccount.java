52c38fab45c79ec83218a8c5030ac5b84b3a4c6e
mensahTribeWeb <ndm.acct85@gmail.com>
2022-06-13 23:30:32 -0500

    codecademy update

--- javaProjects/src/main/java/MANIPULATINGVARIABLES/Project/BankAccount.java
+++ javaProjects/src/main/java/MANIPULATINGVARIABLES/Project/BankAccount.java
@@ -1,7 +1,7 @@
 package MANIPULATINGVARIABLES.Project;
 
 public class BankAccount {
-    
+
         public static void main(String[] args){
             double balance = 1000.75;
             double amountToWithdraw = 250;
